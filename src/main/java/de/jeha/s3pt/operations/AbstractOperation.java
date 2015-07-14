add57a693eb6cce1d3d4f620d1a4ce5842b837e6
jenshadlich <jenshadlich@googlemail.com>
2015-07-14 08:21:32 +0200

    remove static logger

--- src/main/java/de/jeha/s3pt/operations/AbstractOperation.java
+++ src/main/java/de/jeha/s3pt/operations/AbstractOperation.java
@@ -2,8 +2,6 @@ package de.jeha.s3pt.operations;
 
 import de.jeha.s3pt.OperationResult;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Callable;
 
@@ -11,8 +9,6 @@ import java.util.concurrent.Callable;
  * @author jenshadlich@googlemail.com
  */
 public abstract class AbstractOperation implements Callable<OperationResult> {
-
-    private final static Logger LOG = LoggerFactory.getLogger(AbstractOperation.class);
 
     private final DescriptiveStatistics stats = new DescriptiveStatistics();
 
