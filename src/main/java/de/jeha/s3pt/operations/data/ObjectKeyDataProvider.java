a430cb9ab3dd25f41966c6b769d46b57420cfa22
Jens Hadlich <jenshadlich@googlemail.com>
2015-07-26 20:20:23 +0200

    add data provider for object keys

---
+++ src/main/java/de/jeha/s3pt/operations/data/ObjectKeyDataProvider.java
@@ -0,0 +1,12 @@
+package de.jeha.s3pt.operations.data;
+
+import java.util.Map;
+
+/**
+ * @author jenshadlich@googlemail.com
+ */
+public interface ObjectKeyDataProvider {
+
+    Map<Integer, String> get();
+
+}
