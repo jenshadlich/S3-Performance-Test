da1ab7d9bcfcf7e3333d4b0cf7b6e37dd2512f51
Jing Tan <jingyoko@gmail.com>
2018-02-25 07:57:41 -0500

    added delete operation

--- src/main/java/de/jeha/s3pt/Operation.java
+++ src/main/java/de/jeha/s3pt/Operation.java
@@ -7,6 +7,7 @@ public enum Operation {
 
     CLEAR_BUCKET(false),
     CREATE_BUCKET(false),
+    DELETE_BUCKET(false),
     CREATE_KEY_FILE(false),
     RANDOM_READ(true),
     RANDOM_READ_METADATA(true),
